fc088b31b75240dd388efee009479d5e2d8ba912
Hossein Zahed <Hossein.Zahed@maersk.com>
2021-03-17 15:37:18 +0100

    Linq benchmark project added.

---
+++ LinqSample/LinqBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSample
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class LinqBenchmarks
+    {
+        private readonly Dictionary<int, string> _employees = new Dictionary<int, string>
+        {
+            { 1, "Bence" },
+            { 2, "Jazeem" },
+            { 3, "Shahid" },
+            { 4, "Luca" },
+            { 5, "Hossein" }
+        };
+
+        [Benchmark]
+        public void GetEmployeeWithWherePredicate()
+        {
+            var employee = _employees.Where(e => e.Key == 3).First();
+        }
+
+        [Benchmark]
+        public void GetEmployeeWithFirstPredicate()
+        {
+            var employee = _employees.First(e => e.Key == 3);
+        }
+    }
+}
---
+++ LinqSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LinqSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<LinqBenchmarks>();
+        }
+    }
+}
